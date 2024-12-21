b6a713c0307a1e3ee5158ac73630cabf8a6ed6e1
Gus <august.colonna@gmail.com>
2024-12-21 20:32:28 +0100

    [0001] fixed style error and tabWidth in prettieryml file.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import App from '@/App.vue'
 // import router from '@/router'
 // import store from '@/store'
-import '@/styles/main.scss'
+// import '@/styles/main.scss'
 import { createApp } from 'vue'
 
 // .use(router).use(store)
